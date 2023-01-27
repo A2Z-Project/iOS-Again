88d2a3537e20648fd73d6997bc3616010920004b
Kim Mingyu <k3h1i013@gmail.com>
2023-01-27 14:06:03 +0900

    iOS Project Init

---
+++ iOS-Again/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  iOS-Again
+//
+//  Created by DDang on 2023/01/27.
+//
+
+import UIKit
+import FirebaseCore
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
---
+++ iOS-Again/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iOS-Again
+//
+//  Created by DDang on 2023/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
