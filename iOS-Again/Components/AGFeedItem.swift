8186ced7325d1b8f96d4abe2f6dc6d6b935821de
Kim Mingyu <k3h1i013@gmail.com>
2023-03-09 00:14:02 +0900

    Change FeedView
    Change Legacy UIScrollView to UITableView

--- iOS-Again/Components/AGFeedItem.swift
+++ iOS-Again/Components/AGFeedItem.swift
@@ -1,131 +1,132 @@
 import UIKit
 import SnapKit
 
-class AGFeedItem: UIStackView {
-    required init() {
-        super.init(frame: .zero)
+class AGFeedItem: UITableViewCell {
+    static let identifier = "agFeedCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        self.axis = .vertical
-        self.spacing = 10
-        
-        self.layoutMargins = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
-        self.isLayoutMarginsRelativeArrangement = true
-        
-        let header: UIStackView = {
+        let wrapper: UIStackView = {
             let stackView = UIStackView()
             
-            stackView.axis = .horizontal
-            stackView.alignment = .leading
+            stackView.axis = .vertical
             stackView.spacing = 10
             
-            let profileImageView: UIImageView = {
-                let imageView = UIImageView()
-                
-                imageView.backgroundColor = UIColor(red: 153 / 255, green: 238 / 255, blue: 1, alpha: 1)
-                imageView.layer.masksToBounds = true
-                imageView.layer.cornerRadius = 22.5
-                
-                return imageView
-            }()
+            stackView.isLayoutMarginsRelativeArrangement = true
+            stackView.layoutMargins = UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
             
-            let profileVerticalStackView: UIStackView = {
-                let pvStackView = UIStackView()
+            let header: UIStackView = {
+                let stackView = UIStackView()
                 
-                pvStackView.axis = .vertical
-                pvStackView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+                stackView.axis = .horizontal
+                stackView.alignment = .leading
+                stackView.spacing = 10
                 
-                let profileNameLabel: UILabel = {
-                    let label = UILabel()
+                let profileImageView: UIImageView = {
+                    let imageView = UIImageView()
                     
-                    label.text = "User Name"
-                    label.font = UIFont(type: NotoSansKR.medium, size: 16)
+                    imageView.backgroundColor = UIColor(red: 153 / 255, green: 238 / 255, blue: 1, alpha: 1)
+                    imageView.layer.masksToBounds = true
+                    imageView.layer.cornerRadius = 22.5
                     
-                    return label
+                    return imageView
                 }()
                 
-                let profileGroupsStackView: UIStackView = {
-                    let stackView = UIStackView()
+                let profileVerticalStackView: UIStackView = {
+                    let pvStackView = UIStackView()
                     
-                    stackView.axis = .horizontal
-                    stackView.alignment = .leading
-                    stackView.spacing = 5
+                    pvStackView.axis = .vertical
+                    pvStackView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
                     
-                    let groupTags: [AGFeedItemGroupTag] = [
-                        AGFeedItemGroupTag("Group Name"),
-                        AGFeedItemGroupTag("Group Name")
-                    ]
-                    
-                    groupTags.forEach { stackView.addArrangedSubview($0) }
-                    stackView.addArrangedSubview({
-                        let spacer = UIView()
+                    let profileNameLabel: UILabel = {
+                        let label = UILabel()
                         
-                        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
+                        label.text = "User Name"
+                        label.font = UIFont(type: NotoSansKR.medium, size: 16)
                         
-                        return spacer
-                    }())
+                        return label
+                    }()
                     
-                    return stackView
+                    let profileGroupsStackView: UIStackView = {
+                        let stackView = UIStackView()
+                        
+                        stackView.axis = .horizontal
+                        stackView.alignment = .leading
+                        stackView.spacing = 5
+                        
+                        let groupTags: [AGFeedItemGroupTag] = [
+                            AGFeedItemGroupTag("Group Name"),
+                            AGFeedItemGroupTag("Group Name")
+                        ]
+                        
+                        groupTags.forEach { stackView.addArrangedSubview($0) }
+                        stackView.addArrangedSubview({
+                            let spacer = UIView()
+                            
+                            spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
+                            
+                            return spacer
+                        }())
+                        
+                        return stackView
+                    }()
+                    
+                    [profileNameLabel, profileGroupsStackView].forEach { pvStackView.addArrangedSubview($0) }
+                    
+                    return pvStackView
                 }()
                 
-                [profileNameLabel, profileGroupsStackView].forEach { pvStackView.addArrangedSubview($0) }
+                let optionButton: UIButton = {
+                    let button = UIButton()
+                    
+                    button.setImage(UIImage(systemName: "ellipsis")?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
+                    button.setBackgroundColor(UIColor(red: 187 / 255, green: 187 / 255, blue: 187 / 255, alpha: 1), for: .normal)
+                    
+                    button.layer.masksToBounds = true
+                    button.layer.cornerRadius = 15
+                    
+                    return button
+                }()
                 
-                return pvStackView
+                [profileImageView, profileVerticalStackView, optionButton].forEach { stackView.addArrangedSubview($0) }
+                
+                profileImageView.snp.makeConstraints { make in
+                    make.width.height.equalTo(45)
+                }
+                
+                optionButton.snp.makeConstraints { make in
+                    make.width.height.equalTo(30)
+                }
+                
+                return stackView
             }()
             
-            let optionButton: UIButton = {
-                let button = UIButton()
+            let contentTexts: UILabel = {
+                let label = UILabel()
                 
-                button.setImage(UIImage(systemName: "ellipsis")?.withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
-                button.setBackgroundColor(UIColor(red: 187 / 255, green: 187 / 255, blue: 187 / 255, alpha: 1), for: .normal)
+                label.text = "여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요."
+                label.font = UIFont(type: NotoSansKR.regular, size: 14)
                 
-                button.layer.masksToBounds = true
-                button.layer.cornerRadius = 15
+                label.numberOfLines = 5
+                label.lineBreakMode = .byCharWrapping
                 
-                return button
+                return label
             }()
             
-            [profileImageView, profileVerticalStackView, optionButton].forEach { stackView.addArrangedSubview($0) }
-            
-            profileImageView.snp.makeConstraints { make in
-                make.width.height.equalTo(45)
-            }
-            
-            optionButton.snp.makeConstraints { make in
-                make.width.height.equalTo(30)
-            }
+            [header, contentTexts].forEach { stackView.addArrangedSubview($0) }
             
             return stackView
         }()
         
-        let contentTexts: UILabel = {
-            let label = UILabel()
-            
-            label.text = "여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요. 여기에 Content를 써줘요."
-            label.font = UIFont(type: NotoSansKR.regular, size: 14)
-            
-            label.numberOfLines = 5
-            label.lineBreakMode = .byCharWrapping
-            
-            return label
-        }()
+        self.addSubview(wrapper)
         
-        let divideLine: UIView = {
-            let line = UIView()
-            
-            line.backgroundColor = UIColor(red: 16 / 255, green: 128 / 255, blue: 128 / 255, alpha: 1)
-            
-            return line
-        }()
-        
-        [header, contentTexts, divideLine].forEach { self.addArrangedSubview($0) }
-        
-        divideLine.snp.makeConstraints { make in
-            make.width.equalTo(header.snp.width)
-            make.height.equalTo(1)
+        wrapper.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
     }
     
-    required init(coder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
--- iOS-Again/Views/FeedViewController.swift
+++ iOS-Again/Views/FeedViewController.swift
@@ -20,6 +20,16 @@ class FeedViewController: UIViewController {
         
         return stackView
     }()
+    let feedView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(AGFeedItem.self, forCellReuseIdentifier: AGFeedItem.identifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 200
+        
+        return tableView
+    }()
     let feeds: [AGFeedItem] = [
         AGFeedItem(),
         AGFeedItem(),
@@ -38,29 +48,48 @@ class FeedViewController: UIViewController {
 
 extension FeedViewController {
     func configureLayout() {
-        [header, scrollView].forEach { self.view.addSubview($0) }
-        scrollView.addSubview(scrollContentView)
-        feeds.forEach { scrollContentView.addArrangedSubview($0) }
+        [header, feedView].forEach { self.view.addSubview($0) }
+//        scrollView.addSubview(scrollContentView)
+//        feeds.forEach { scrollContentView.addArrangedSubview($0) }
         
         header.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.horizontalPaddingToSuperView(0)
         }
         
-        scrollView.snp.makeConstraints { make in
+//        scrollView.snp.makeConstraints { make in
+//            make.top.equalTo(header.snp.bottom)
+//            make.trailing.bottom.leading.equalToSuperview()
+//        }
+//
+//        scrollContentView.snp.makeConstraints { make in
+//            make.edges.equalTo(scrollView.contentLayoutGuide)
+//            make.height.greaterThanOrEqualTo(scrollView.frame.size.height).priority(.low)
+//            make.width.equalTo(scrollView.snp.width)
+//        }
+    
+        feedView.delegate = self
+        feedView.dataSource = self
+        feedView.estimatedRowHeight = 100
+        feedView.snp.makeConstraints { make in
             make.top.equalTo(header.snp.bottom)
             make.trailing.bottom.leading.equalToSuperview()
-        }
-        
-        scrollContentView.snp.makeConstraints { make in
-            make.edges.equalTo(scrollView.contentLayoutGuide)
-            make.height.greaterThanOrEqualTo(scrollView.frame.size.height).priority(.low)
-            make.width.equalTo(scrollView.snp.width)
         }
     }
     
     func didAction() {
         
+    }
+}
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: AGFeedItem.identifier, for: indexPath) as! AGFeedItem
+        return cell
     }
 }
 
